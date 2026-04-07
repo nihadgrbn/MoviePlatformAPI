3bb3f6c2c1985b03161e54b7834096b568b62f7a
nihadgrbn <nihadgurbanli204@gmail.com>
2026-04-07 10:35:29 +0400

    jwt token generation implemented

--- MoviePlatformAPI/Program.cs
+++ MoviePlatformAPI/Program.cs
@@ -1,19 +1,46 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models; 
 using MoviePlatformAPI.Data;
 using MoviePlatformAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+});
+
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-var app = builder.Build();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -23,8 +50,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-app.UseAuthorization();
+app.UseAuthentication(); 
+app.UseAuthorization();  
 
 app.MapControllers();
 
--- MoviePlatformAPI/Services/AuthService.cs
+++ MoviePlatformAPI/Services/AuthService.cs
@@ -1,40 +1,74 @@
-using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MoviePlatformAPI.Data;
 using MoviePlatformAPI.DTOs;
 using MoviePlatformAPI.Models;
 
 namespace MoviePlatformAPI.Services;
 
-public class AuthService: IAuthService
+public class AuthService : IAuthService
 {
     private readonly AppDbContext _context;
-    public AuthService(AppDbContext context)
+    private readonly IConfiguration _configuration;
+
+    public AuthService(AppDbContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
-    
+
     public async Task<User?> Register(UserRegisterDto request)
     {
-        if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+        if (await _context.Users.AnyAsync(u => u.Username == request.Username || u.Email == request.Email))
             return null;
-        string? passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
         var user = new User
         {
             Username = request.Username,
             Email = request.Email,
             PasswordHash = passwordHash
         };
+
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
+
         return user;
     }
 
     public async Task<string?> Login(UserLoginDto request)
     {
         var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+        
         if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
             return null;
-        return "User successfully logged in";
+
+        return CreateToken(user);
+    }
+
+    private string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value!));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1), 
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
